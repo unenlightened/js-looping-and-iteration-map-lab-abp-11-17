e201431cabfb4a6ed73d00e95c7c4bf300b0d15d
Chely Ho <adept_inept@yahoo.com>
2017-11-07 15:27:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(collection){
+  return collection.map(function(num) {
+    return num.toLowerCase();
+  });
+}
+
+function nameToAttributes(collection){        //doesn't consider double names, middle names, titles or initials
+  return collection.map(function(num) {
+    let name = num.split(' ');
+    return Object.assign({}, { firstName: name[0] }, { lastName: name[1] } );
+  });
+}
+
+function attributesToPhrase(collection){
+  return collection.map(function(num){
+    return num.name + ' is from ' + num.hometown;
+  });
+}
